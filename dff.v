2e101b7a2916f43b84d5fea5f3f61cb2e3d7d9a9
avinash kanaujia <75214974+avinashavi20@users.noreply.github.com>
2021-06-10 06:39:04 -0700

    Add files via upload

---
+++ dff.v
@@ -0,0 +1,45 @@
+/********************************************************************************************
+
+Copyright 2018-2019 - Maven Silicon Softech Pvt Ltd. All Rights Reserved.
+
+This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
+It is considered a trade secret and is not to be divulged or used by parties who 
+have not received written authorization from Maven Silicon Softech Pvt Ltd.
+
+Maven Silicon Softech Pvt Ltd
+Bangalore - 560076
+
+Webpage: www.maven-silicon.com
+
+Filename:	dff.v   
+
+Description:	Delay flip flop 
+
+Date:		01/05/2018
+
+Author:		Maven Silicon
+
+Email:		online@maven-silicon.com
+		 
+
+Version:	1.0
+
+*********************************************************************************************/
+ 
+module d_ff(clk,
+           reset,
+           d,
+           q,
+           qb);
+
+ // Step1 : Declare Port Directions
+ // Step2 : Write the behavioral logic for D flip-flop functionality.
+
+ // Step3: Assign complement of q to qb.
+
+       
+
+ 
+endmodule         
+
+
